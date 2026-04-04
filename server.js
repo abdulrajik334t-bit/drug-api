4690d2df035380b081c82af646f40b9705e2f91a
abdulrajik334t-bit <abdulrajik334t@gmail.com>
2026-04-04 17:46:47 +0530

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI("YOUR_GEMINI_API_KEY_HERE");
+const genAI = new GoogleGenerativeAI("AIzaSyDqtPDRM45OxrFQzPCYWcUE3OrTMxm0XfU");
 const axios = require("axios");
 const multer = require("multer");
 const Tesseract = require("tesseract.js");
